a926cb2c7343c51eb8e30082ea43676ab8b21e0e
samavati <samaavaati@gmail.com>
2022-04-07 15:48:38 +0430

    docs: nav bug fixed

--- docs/src/assets/js/docs.js
+++ docs/src/assets/js/docs.js
@@ -12,45 +12,49 @@ const sidebarLinks = document.querySelectorAll('#docs-sidebar .scrollto');
 
 /* ===== Responsive Sidebar ====== */
 
-window.onload=function() 
-{ 
-    responsiveSidebar(); 
+window.onload = function () {
+	if (sidebar) {
+		responsiveSidebar();
+	}
 };
 
-window.onresize=function() 
-{ 
-    responsiveSidebar(); 
+window.onresize = function () {
+	if (sidebar) {
+		responsiveSidebar();
+	}
 };
 
 
 function responsiveSidebar() {
-    let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    console.log('larger');
+	let w = window.innerWidth;
+	if (w >= 1200) {
+		// if larger 
+		console.log('larger');
 		sidebar.classList.remove('sidebar-hidden');
 		sidebar.classList.add('sidebar-visible');
-		
+
 	} else {
-	    // if smaller
-	    console.log('smaller');
-	    sidebar.classList.remove('sidebar-visible');
+		// if smaller
+		console.log('smaller');
+		sidebar.classList.remove('sidebar-visible');
 		sidebar.classList.add('sidebar-hidden');
 	}
 };
 
-sidebarToggler.addEventListener('click', () => {
-	if (sidebar.classList.contains('sidebar-visible')) {
-		console.log('visible');
-		sidebar.classList.remove('sidebar-visible');
-		sidebar.classList.add('sidebar-hidden');
-		
-	} else {
-		console.log('hidden');
-		sidebar.classList.remove('sidebar-hidden');
-		sidebar.classList.add('sidebar-visible');
-	}
-});
+if (sidebarToggler) {
+	sidebarToggler.addEventListener('click', () => {
+		if (sidebar.classList.contains('sidebar-visible')) {
+			console.log('visible');
+			sidebar.classList.remove('sidebar-visible');
+			sidebar.classList.add('sidebar-hidden');
+
+		} else {
+			console.log('hidden');
+			sidebar.classList.remove('sidebar-hidden');
+			sidebar.classList.add('sidebar-visible');
+		}
+	});
+}
 
 
 /* ===== Smooth scrolling ====== */
@@ -58,27 +62,27 @@ sidebarToggler.addEventListener('click', () => {
 /* Ref: https://github.com/iamdustan/smoothscroll */
 
 sidebarLinks.forEach((sidebarLink) => {
-	
+
 	sidebarLink.addEventListener('click', (e) => {
-		
+
 		e.preventDefault();
-		
+
 		var target = sidebarLink.getAttribute("href").replace('#', '');
-		
+
 		//console.log(target);
-		
-        document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
-        
-        
-        //Collapse sidebar after clicking
-		if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200){
-			
+
+		document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
+
+
+		//Collapse sidebar after clicking
+		if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200) {
+
 			sidebar.classList.remove('sidebar-visible');
-		    sidebar.classList.add('sidebar-hidden');
-		} 
-		
-    });
-	
+			sidebar.classList.add('sidebar-hidden');
+		}
+
+	});
+
 });
 
 
