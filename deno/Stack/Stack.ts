31a9320e1f2f64372f6fcbf20d23d230b2829734
Ehsan Samavati <samaavaati@gmail.com>
2022-08-21 14:50:34 +0300

    chore(deno): Automatic deno files generated from src folder

--- deno/Stack/Stack.ts
+++ deno/Stack/Stack.ts
@@ -53,7 +53,7 @@ export class Stack<T = any> {
   }
 
   /**
-   * Gets the number of elements contained in the **Stack<T>**.
+   * Gets the number of elements contained in the `Stack<T>`.
    * @example
    * ```typescript
    * const stack = new Stack<number>();
@@ -64,7 +64,7 @@ export class Stack<T = any> {
    *
    * stack.length // => 3
    * ```
-   * @remarks Retrieving the value of this property is an O(1) operation.
+   * @remarks Retrieving the value of this property is an **O(1)** operation.
    * @returns {Number} length of Stack<T>
    * @memberof Stack
    * @name length
